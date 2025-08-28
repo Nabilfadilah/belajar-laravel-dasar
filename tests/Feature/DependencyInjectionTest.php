257b5d2a5c0b037ea88b2dc5675a713875a05dcf
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-08-28 13:05:03 +0700

    Dependency injection dan menjalankan unit test

---
+++ tests/Feature/DependencyInjectionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class DependencyInjectionTest extends TestCase
+{
+    // public function testDependencyInjection()
+    // {
+    //     $foo = new Foo();
+    //     $bar = new Bar($foo); // melakukan injection data si Foo
+
+    //     self::assertEquals('Foo and Bar', $bar->bar());
+    // }
+}

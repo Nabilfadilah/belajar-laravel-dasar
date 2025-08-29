1229c4f37911abebd819e4e230572655e903219e
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-08-29 19:06:49 +0700

    Storage link dan menjalankan unit test

---
+++ tests/Feature/FileStorageTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class FileStorageTest extends TestCase
+{
+    public function testStorage()
+    {
+        $filesystem = Storage::disk("local");
+        $filesystem->put("file.txt", "Put Mohammad Nabil Fadilah");
+
+        self::assertEquals("Put Mohammad Nabil Fadilah", $filesystem->get("file.txt"));
+    }
+
+    public function testPublic()
+    {
+        $filesystem = Storage::disk("public");
+        $filesystem->put("file.txt", "Put Mohammad Nabil Fadilah");
+
+        $content = $filesystem->get("file.txt");
+
+        self::assertEquals("Put Mohammad Nabil Fadilah", $content);
+    }
+}
